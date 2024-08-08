852e9d1f13a91b4840fc861b15e5c7e5df67a3b3
aaguirre <alejandra36a@gmail.com>
2024-08-08 22:44:00 +0000

    cambiossss casii finales

--- src/js/component/formularioTareas.jsx
+++ src/js/component/formularioTareas.jsx
@@ -7,27 +7,41 @@ const FormularioTareas = () => {
     const [toDoList, setToDoList] = useState([]);
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
-
-            toDoList.push(inputValor);
-            setToDoList(toDoList);
+            if (inputValor.trim().length > 0) {
+                toDoList.push(inputValor);
+                setToDoList(toDoList);
+            }
+            else {
+                alert('Introduzca una tarea correcta')
+            }
             setInputValor("");
-
         }
+
     }
     return (
-        <ul className="formulario-tareas" >
-            <input
-                type="texto"
-                className="formulario-tareas__input"
-                placeholder="Ingresa tu tarea"
-                value={inputValor}
-                onChange={(e) => setInputValor(e.target.value)}
-                onKeyDown={handleKeyPress}
-            />
+        <div>
+            <ul className="formulario-tareas" >
+                <input
+                    type="texto"
+                    className="formulario-tareas__input"
+                    placeholder="Ingresa tu tarea"
+                    value={inputValor}
+                    onChange={(e) => setInputValor(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                />
 
-            {toDoList.map((a, index) => <li key={index}>{a}</li>)}
+                {toDoList.map((a, index) => <li className="tareas-creadas" key={index}>{a}{" "} <i className="fa fa-solid fa-trash"
+                    onClick={() => setToDoList(toDoList.filter((u, uIndex) => index != uIndex))}></i></li>)}
 
-        </ul>
+            </ul>
+            {
+             toDoList.length == 1 && <div className="valores-tarea">Tienes {toDoList.length} tarea</div>
+            }
+            {
+             toDoList.length != 1 && <div className="valores-tarea">Tienes {toDoList.length} tareas</div>
+            }
+            
+        </div>
 
     );
 }
