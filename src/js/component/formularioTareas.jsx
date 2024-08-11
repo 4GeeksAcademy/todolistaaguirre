b2272059d71f28deb4b08a30f983a211b57859ad
aaguirre <alejandra36a@gmail.com>
2024-08-11 22:50:01 +0000

    final

--- src/js/component/formularioTareas.jsx
+++ src/js/component/formularioTareas.jsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 
+
 const FormularioTareas = () => {
+    //Esta constante sirve para forzar el renderizado y que se actualice el HTML con el valor que se va modificando
+    const [ignored, forceUpdate] = React.useReducer(x => x + 1, 0);
 
+    const Modificar = (value, index) => {
 
-   
+        toDoList[index] = value;
+
+        forceUpdate();
+
+    }
 
 
     const [inputValor, setInputValor] = useState('');
-
+    const [actualizar, setActualizar] = useState(true);
     const [toDoList, setToDoList] = useState([]);
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
+
             if (inputValor.trim().length > 0) {
                 toDoList.push(inputValor);
-                setToDoList(toDoList);
             }
             else {
                 alert('Introduzca una tarea correcta')
             }
             setInputValor("");
+            setActualizar(true);
         }
     }
     return (
@@ -32,16 +41,27 @@ const FormularioTareas = () => {
                     onChange={(e) => setInputValor(e.target.value)}
                     onKeyDown={handleKeyPress}
                 />
-        
+
                 {toDoList.map((a, index) =>
-                    <li className="tareas-creadas" key={index}>{a}
+                    <div className=" tareas-creadas">
+                        <input
+                            className="inputModificar"
+                            type="texto"
+                            placeholder={toDoList[index]}
+                            value={a}
+                            key={index}
+                            onChange={(e) => Modificar(e.target.value, index)}
+                            disabled={actualizar} />
+
                         <div className="iconos">
-                        <i className="faa fa-solid fa-trash"></i>
-                        <i className="fa fa-solid fa-trash"
-                            onClick={() => setToDoList(toDoList.filter((u, uIndex) => index != uIndex))} > </i>
-                        
+                            <button className="boton" type="submit" disabled={actualizar} onClick={() => setActualizar(true)}>Actualizar</button>
+                            <i className=" fa fa-solid fa-pen" onClick={() => setActualizar(false)}></i>
+                            <i className="fa fa-solid fa-trash"
+                                onClick={() => setToDoList(toDoList.filter((u, uIndex) => index != uIndex))} > </i>
+
                         </div>
-                    </li>)}
+                    </div>
+                )}
 
             </ul>
             {
@@ -51,7 +71,7 @@ const FormularioTareas = () => {
                 toDoList.length != 1 && <div className="valores-tarea">Tienes {toDoList.length} tareas</div>
             }
 
-        </div>
+        </div >
 
     );
 }
